0ee2de58976eeee258cfbd7e3042446b87aacfec
Szesz96 <szesza.ka@wp.pl>
2023-01-14 16:58:11 +0100

    img folder added and Main edited

--- src/views/Main.jsx
+++ src/views/Main.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 const Main = () => {
   return (
     <>
-      <img src="" alt="" />
+    <div>
+      <img src="../img/main-img.png" alt="" />
+    </div>
+    <div>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum sapiente, unde ad, ea hic dicta quasi eaque dolores, earum iste eius optio reiciendis culpa tenetur necessitatibus commodi! Perferendis, quis tenetur?Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa enim officia, inventore blanditiis mollitia autem.</p>
+    </div>
     </>
   )
 }
