5311fcd15aa7523b4b5d0c9bba06bb1f567f449f
Szesz96 <szesza.ka@wp.pl>
2023-01-14 16:35:42 +0100

    little changes to views

--- src/views/Main.jsx
+++ src/views/Main.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
-import Navbar from '../components/Navbar'
-import Footer from '../components/Footer'
 
 const Main = () => {
   return (
     <>
-      
+      <img src="" alt="" />
     </>
   )
 }
--- src/views/relax.jsx
+++ src/views/relax.jsx
@@ -1,7 +1,5 @@
 
 import React from 'react'
-import Footer from '../components/Footer'
-import Navbar from '../components/Navbar'
 import Affirmations from "../components/affirmations"
 import Meditation from "../components/meditation"
 import Mindfulness from "../components/mindfulness"
