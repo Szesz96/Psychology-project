5d146510a45f4529ac4270a3f1e48b6c4cd0a2b3
martazientara <marta.zientara@gmail.com>
2023-01-15 16:17:44 +0100

    deleted

--- src/components/mindfulness.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Mindfulness () {
-    return (
-        <h1 className="section-title">Ćwiczenia uważności</h1>
-    );
-}
-
-export default Mindfulness;
--- src/components/movement.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Movement () {
-    return (
-        <h1 className="section-title">Ruch to zdrowie</h1>
-    );
-}
-
-export default Movement;
