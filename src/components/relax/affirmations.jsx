0fe1e45b2b5b4de6ae43703d446738ca3743265b
martazientara <marta.zientara@gmail.com>
2023-01-22 15:11:41 +0100

    styles in relaksownik

--- src/components/relax/affirmations.jsx
+++ src/components/relax/affirmations.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import {useState} from 'react';
 import './relax.css'
 import affirmationData from './affirmationData'
-import Photo from './melissa.jpg'
+import Photo from './happy-kid.jpg'
 
 function Affirmations () {
     const [oneAffirmation, setAffirmation] = useState("");
@@ -24,10 +24,10 @@ function Affirmations () {
                 <div className='text-about'>
                 <div>Afirmacje są krótkimi podstawowymi założeniami deklaracjami, przekonaniami, zdaniami, aprobatami, które wyrażają postawę lub przekonanie na temat siebie, lub okoliczności swojego życia. Są podstawą codziennego myślenia, od którego wszystko się zaczyna. Sposób, w jaki myślisz o sobie, innych ludziach, wydarzeniach i świecie, jest tym, jak się czujesz. To, jak się czujesz, jest tym, jak się zachowujesz.</div>
                 <div>Gdy ktoś pyta, czy afirmacje dla dzieci w ogóle mają sens, odpowiadamy tak: słowa przekładają się na myśli, myśli wpływają na uczucia, uczucia – na zachowania, zachowania zmieniają się w nawyki, a te kształtują całe życie. Więc tak: afirmacje dla dzieci są potężnym narzędziem, które już od najmłodszych lat kierunkuje człowieka na rozwój, trwanie w pozytywnych wibracjach i odczuwanie wdzięczności zamiast presji. Zobacz, jakie afirmacje dla dzieci warto stosować.</div>
-                </div>     
+            </div>     
         </div>
-            <div className='affirmation-result'>{oneAffirmation}</div>
                 <button className='affirmation-button' onClick={getAffirmation}>WYLOSUJ AFIRMACJĘ NA DZIŚ</button>
+                <div className='affirmation-result'>{oneAffirmation}</div>
             </div>
         
        
--- src/components/relax/meditation.jsx
+++ src/components/relax/meditation.jsx
@@ -1,17 +1,28 @@
 import React from 'react'
 import './relax.css'
+import Photo from './melissa.jpg'
 
 function Meditation () {
     return (
         <>
         <h2 className="section-title">Medytacje</h2>
-        <div className="about">
-            Medytacja to trening umysłu, który zawiera elementy relaksacji, skupienia oraz świadomości. Medytacja dla umysłu jest tym, czym ćwiczenia fizyczne są dla ciała. Praktyka zazwyczaj jest wykonywana indywidualnie w pozycji siedzącej oraz z zamkniętymi oczami. Celem medytacji jest budowanie bardziej harmonijnego życia i lepszego zrozumienia siebie i innych. Dzięki temu możemy mieć więcej jasności w życiu i łatwiej podejmować decyzję. A to przyczynia się do szczęśliwego życia. Medytacja to proces, to umiejętność, którą trzeba trenować. Podobnie jak z każdą inną umiejętnością np. nauką angielskiego, żeby mieć efekty trzeba ćwiczyć. Nie wystarczy ćwiczyć raz przez kilka godzin, trzeba stopniowo rozwijać umiejętność — ćwicząc np. kilka razy w tygodniu. A efekty na pewno się pojawią.
+        <div className="about-aff">
+            <div className="text-about">Medytacja to trening umysłu, który zawiera elementy relaksacji, skupienia oraz świadomości. Medytacja dla umysłu jest tym, czym ćwiczenia fizyczne są dla ciała. Praktyka zazwyczaj jest wykonywana indywidualnie w pozycji siedzącej oraz z zamkniętymi oczami. Celem medytacji jest budowanie bardziej harmonijnego życia i lepszego zrozumienia siebie i innych. Dzięki temu możemy mieć więcej jasności w życiu i łatwiej podejmować decyzję. A to przyczynia się do szczęśliwego życia. Medytacja to proces, to umiejętność, którą trzeba trenować. Podobnie jak z każdą inną umiejętnością np. nauką angielskiego, żeby mieć efekty trzeba ćwiczyć. Nie wystarczy ćwiczyć raz przez kilka godzin, trzeba stopniowo rozwijać umiejętność — ćwicząc np. kilka razy w tygodniu. A efekty na pewno się pojawią.</div>
+            <img className='single-photo' src={Photo}></img>
         </div>
+        <h3>Skorzystaj z naszych medytacji:</h3>
         <div className="section-video">
-            <iframe className="single-video" src="https://www.youtube.com/embed/NX6xjjZBiDc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-            <iframe className="single-video" src="https://www.youtube.com/embed/NX6xjjZBiDc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-            <iframe className="single-video" src="https://www.youtube.com/embed/NX6xjjZBiDc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            <div className="single-video-tile">
+                <p className="meditation-title">Medytacja wewnętrznej równowagi</p>
+                <iframe className="single-video" id="v1" src="https://www.youtube.com/embed/NX6xjjZBiDc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></div>
+            <div className="single-video-tile">
+                <p className="meditation-title">Medytacja wewnętrznej równowagi</p>
+                <iframe className="single-video" id="v2" src="https://www.youtube.com/embed/NX6xjjZBiDc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            </div>
+            <div className="single-video-tile">
+                <p className="meditation-title">Medytacja wewnętrznej równowagi</p>
+                <iframe className="single-video" id="v3" src="https://www.youtube.com/embed/NX6xjjZBiDc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            </div>
         </div>
         <hr className="break-between"/>
         </>
