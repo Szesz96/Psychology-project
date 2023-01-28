b276dc371ac5d1db57b8032941bb4e8fcb832d87
martazientara <marta.zientara@gmail.com>
2023-01-28 14:15:41 +0100

    changes in relax

--- src/components/relax/relax-start.jsx
+++ src/components/relax/relax-start.jsx
@@ -1,34 +1,44 @@
-import React from 'react'
-import './relax.css'
-// import Photo from "./catalin-pop-noydSJIWMSg-unsplash.jpg"
+// import React from 'react'
+// import './relax.css'
+// import { useRef } from 'react';
+// // import Photo from "./catalin-pop-noydSJIWMSg-unsplash.jpg"
 
 
-function Start () {
-    return (
-        <>
-        <div className="main-photo">
-        <h1>Relaksownik dziecięcy</h1>
-        </div>
-        <div className="relax-container">
-            {/* <img className="main-photo-single" src={Photo} /> */}
-            <div className="about-relax">
+
+// function Start () {
+//     const mindfulness = useRef(null);
+//     const scrollToSection = (elementRef) => {
+//         window.scrollTo({
+//             top: elementRef.current.offsetTop,
+//             behavior: "smooth"
+//         })
+//     }
+   
+//     return (
+//         <>
+//         <div className="main-photo">
+//         <h1>Relaksownik dziecięcy</h1>
+//         </div>
+//         <div className="relax-container">
+//             {/* <img className="main-photo-single" src={Photo} /> */}
+//             <div className="about-relax">
                 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores in laudantium, nihil vero distinctio veritatis amet expedita commodi sint impedit quis dolorem nostrum possimus quidem, quibusdam iure obcaecati assumenda rerum. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores in laudantium, nihil vero distinctio veritatis amet expedita commodi sint impedit quis dolorem nostrum possimus quidem, quibusdam iure obcaecati assumenda rerum.</p>
-                <div className="go-to">
-                <p className="go-paragraph">Przejdź do:</p>
-                <ul className="relax-list">
-                    <li className="direct">#afirmacje</li>
-                    <li className="direct">#medytacje</li>
-                    <li className="direct">#mindfulness</li>
-                    <li className="direct">#ruch to zdrowie</li>
-                </ul>
-                </div>
-            </div>
-        </div>
-        <hr className="break-between"/>
-        </>
-    );
-}
+//                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores in laudantium, nihil vero distinctio veritatis amet expedita commodi sint impedit quis dolorem nostrum possimus quidem, quibusdam iure obcaecati assumenda rerum. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores in laudantium, nihil vero distinctio veritatis amet expedita commodi sint impedit quis dolorem nostrum possimus quidem, quibusdam iure obcaecati assumenda rerum.</p>
+//                 <div className="go-to">
+//                 <p className="go-paragraph">Przejdź do:</p>
+//                 <ul className="relax-list">
+//                     <li className="direct" onClick={()=>scrollToSection(mindfulness)}>#afirmacje</li>
+//                     <li className="direct" onClick={()=>scrollToSection(mindfulness)}>#medytacje</li>
+//                     <li className="direct" onClick={()=>scrollToSection(mindfulness)}>#mindfulness</li>
+//                     <li className="direct" onClick={()=>scrollToSection(mindfulness)}>#ruch to zdrowie</li>
+//                 </ul>
+//                 </div>
+//             </div>
+//         </div>
+//         <hr className="break-between"/>
+//         </>
+//     );
+// }
 
-export default Start;
+// export default Start;
 
--- src/views/relax/relax.jsx
+++ src/views/relax/relax.jsx
@@ -1,18 +1,41 @@
 
-import React from 'react'
+import React, { useRef } from 'react'
 import Affirmations from "../../components/relax/affirmations"
 import Meditation from "../../components/relax/meditation"
 import Mindfulness from "../../components/relax/mindfulness"
 import Movement from "../../components/relax/movement"
-import Start from "../../components/relax/relax-start"
-
-
 
 
 function Relax () {
+    const mindfulness = useRef(null);
+    const scrollToSection = (elementRef) => {
+        window.scrollTo({
+            top: elementRef.current.offsetTop,
+            behavior: "smooth"
+        })}
+
     return (
         <>
-        <Start />
+        <div className="main-photo">
+        <h1>Relaksownik dziecięcy</h1>
+        </div>
+        <div className="relax-container">
+            {/* <img className="main-photo-single" src={Photo} /> */}
+            <div className="about-relax">
+                
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores in laudantium, nihil vero distinctio veritatis amet expedita commodi sint impedit quis dolorem nostrum possimus quidem, quibusdam iure obcaecati assumenda rerum. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores in laudantium, nihil vero distinctio veritatis amet expedita commodi sint impedit quis dolorem nostrum possimus quidem, quibusdam iure obcaecati assumenda rerum.</p>
+                <div className="go-to">
+                <p className="go-paragraph">Przejdź do:</p>
+                <ul className="relax-list">
+                    <li className="direct" onClick={()=>scrollToSection(mindfulness)}>#afirmacje</li>
+                    <li className="direct" onClick={()=>scrollToSection(mindfulness)}>#medytacje</li>
+                    <li className="direct" onClick={()=>scrollToSection(mindfulness)}>#mindfulness</li>
+                    <li className="direct" onClick={()=>scrollToSection(mindfulness)}>#ruch to zdrowie</li>
+                </ul>
+                </div>
+            </div>
+        </div>
+        <hr className="break-between"/>
         <Affirmations />
         <Meditation />
         <Mindfulness />
